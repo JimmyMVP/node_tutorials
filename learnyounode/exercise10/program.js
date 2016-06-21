28d4f8a1e72e86e906825c84b6d776e05e8b5b0e
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-06-21 11:01:38 +0200

    Exercise 10 solved

---
+++ learnyounode/exercise10/program.js
@@ -0,0 +1,10 @@
+var net = require("net");
+var strftime = require("strftime")
+
+server = net.createServer(function(socket) {
+
+	socket.end(strftime("%Y-%m-%d %H:%M") + "\n")
+
+});
+
+server.listen(process.argv[2]);
